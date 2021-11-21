d4fc760282f083f9170b0238b968f07176af5168
Edward Wang <eddywang0731@gmail.com>
2021-11-21 15:32:55 -0600

    working on quakcdelivery teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/competition/TeleOperated.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/competition/TeleOperated.java
@@ -17,12 +17,12 @@ import org.firstinspires.ftc.teamcode.tasks.GamepadDriveTask;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-@TeleOp(name = "TeleOp", group = "Working Title")
+@TeleOp(name = "QuackDelivery TeleOp", group = "Quackology")
 public class TeleOperated extends SequoiaOpMode {
     private final Mecanum drivetrain = new Mecanum();
-    private final Arm arm = new Arm();
-    private final Rotator rotator = new Rotator();
-    private final Gripper gripper = new Gripper();
+//    private final Arm arm = new Arm();
+//    private final Rotator rotator = new Rotator();
+//    private final Gripper gripper = new Gripper();
 
     @Override
     public void initTriggers() {
@@ -32,24 +32,24 @@ public class TeleOperated extends SequoiaOpMode {
     public void runTriggers() {
         gamepad1H.sticksButton(0.01).onPressWithCancel(new GamepadDriveTask(gamepad1, drivetrain));
 
-        gamepad1H.leftButton().onPress(new InstantTask(() -> arm.setMode(Arm.ArmMode.HORIZONTAL)));
-        gamepad1H.rightButton().onPress(new InstantTask(() -> arm.setMode(Arm.ArmMode.VERTICAL)));
-        gamepad1H.leftBumperButton().onPress(new InstantTask(() -> {
-            arm.setMode(Arm.ArmMode.HOME);
-            gripper.setState(Gripper.GripperState.CLOSED);
-        }));
-
-        gamepad1H.leftTriggerButton(0.01).whilePressed(new InstantTask(() -> arm.modifySetpoint(-0.75 * gamepad1.left_trigger)));
-        gamepad1H.rightTriggerButton(0.01).whilePressed(new InstantTask(() -> arm.modifySetpoint(0.75 * gamepad1.right_trigger)));
-
-        AtomicInteger rotationdir = new AtomicInteger(1);
-        gamepad1H.aToggleButton().risingWithCancel(new StartEndTask(() -> {
-            rotator.setSetpoint(10 * rotationdir.get());
-        }, () -> {
-            rotator.setSetpoint(0);
-            rotationdir.updateAndGet(v -> v * -1);
-        }));
-
-        gamepad1H.bToggleButton().risingWithCancel(new StartEndTask(() -> gripper.setState(Gripper.GripperState.OPEN), () -> gripper.setState(Gripper.GripperState.CLOSED)));
+//        gamepad1H.leftButton().onPress(new InstantTask(() -> arm.setMode(Arm.ArmMode.HORIZONTAL)));
+//        gamepad1H.rightButton().onPress(new InstantTask(() -> arm.setMode(Arm.ArmMode.VERTICAL)));
+//        gamepad1H.leftBumperButton().onPress(new InstantTask(() -> {
+//            arm.setMode(Arm.ArmMode.HOME);
+//            gripper.setState(Gripper.GripperState.CLOSED);
+//        }));
+//
+//        gamepad1H.leftTriggerButton(0.01).whilePressed(new InstantTask(() -> arm.modifySetpoint(-0.75 * gamepad1.left_trigger)));
+//        gamepad1H.rightTriggerButton(0.01).whilePressed(new InstantTask(() -> arm.modifySetpoint(0.75 * gamepad1.right_trigger)));
+//
+//        AtomicInteger rotationdir = new AtomicInteger(1);
+//        gamepad1H.aToggleButton().risingWithCancel(new StartEndTask(() -> {
+//            rotator.setSetpoint(10 * rotationdir.get());
+//        }, () -> {
+//            rotator.setSetpoint(0);
+//            rotationdir.updateAndGet(v -> v * -1);
+//        }));
+//
+//        gamepad1H.bToggleButton().risingWithCancel(new StartEndTask(() -> gripper.setState(Gripper.GripperState.OPEN), () -> gripper.setState(Gripper.GripperState.CLOSED)));
     }
 }
