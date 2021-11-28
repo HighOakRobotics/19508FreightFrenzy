582e8e231ad7371fa4eab6acb7507e7190007295
Staykool123 <eddywang0731@gmail.com>
2021-11-28 15:25:33 -0600

    autoblueleft things (wip)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/EddyAutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/EddyAutoBlue.java
@@ -9,12 +9,17 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+<<<<<<< Updated upstream:TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/EddyAutoBlue.java
 public class EddyAutoBlue {
     /*
     private final DcMotorEx frontLeft;
     private final DcMotorEx backLeft;
     private final DcMotorEx frontRight;
     private final DcMotorEx backRight;
+=======
+public class EddyRotator {
+    DcMotorEx rotator;
+>>>>>>> Stashed changes:TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/EddyRotator.java
 
     private static final double TICKS_PER_MOTOR_REV     = 537.7; //Yellow Jacket motor on rev 320 gear box
     private static final double WHEEL_DIAMETER_INCHES   = 1.8898*2;
@@ -24,18 +29,15 @@ public class EddyAutoBlue {
     private Telemetry telemetry;
     private ElapsedTime runtime;
 
-    public DriveTrainMecanum(HardwareMap hardwareMap, Telemetry telemetry) {
-        frontLeft = hardwareMap.get(DcMotorEx.class, "frontleft");
-        backLeft = hardwareMap.get(DcMotorEx.class, "backleft");
-        backRight = hardwareMap.get(DcMotorEx.class, "backright");
-        frontRight = hardwareMap.get(DcMotorEx.class, "frontright");
+    public EddyRotator(HardwareMap hardwareMap, Telemetry telemetry) {
+        rotator = hardwareMap.get(DcMotorEx.class, "rotator");
 
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        //frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         this.telemetry = telemetry;
         runtime = new ElapsedTime();
+    }
+
+    public EddyRotator() {
     }
 
     public void autoInit() {
@@ -43,54 +45,30 @@ public class EddyAutoBlue {
     }
 
     public void humanControl(Gamepad gamepad) {
-        double forward = -gamepad.left_stick_y;
-        double strafe = gamepad.left_stick_x;
-        double turn = gamepad.right_stick_x;
-
-        drive(forward, strafe, turn);
 
     }
 
-    public double getFL() {return frontLeft.getPower();}
-    public double getBL() {return backLeft.getPower();}
-    public double getFR() {return frontRight.getPower();}
-    public double getBR() {return backRight.getPower();}
+    public double getR() {return rotator.getPower();}
 
     public void stop() {
-        setDrivePowers(0.0, 0.0, 0.0, 0.0);
+        setDrivePowers(0.0);
     }
 
-    public void setDrivePowers(double fl, double bl, double br, double fr) {
-        frontLeft.setPower(fl);
-        backLeft.setPower(bl);
-        backRight.setPower(br);
-        frontRight.setPower(fr);
-    }
-
-    public void drive (double forward, double strafe, double turn)
-    {
-        double fl = forward + strafe + turn;
-        double bl = forward - strafe + turn;
-        double fr = forward - strafe - turn;
-        double br = forward + strafe - turn;
-
-        setDrivePowers(fl, bl, br, fr);
+    public void setDrivePowers(double r) {
+        rotator.setPower(r);
     }
 
     public void forwardByInch (double inches, double power)
     {
         int targetPos = (int)(inches * TICKS_PER_INCH);
 
-        setDrivePowers(power, power, power, power);
+        setDrivePowers(power);
 
-        frontLeft.setTargetPosition(frontLeft.getCurrentPosition() + targetPos);
-        frontRight.setTargetPosition(frontRight.getCurrentPosition() + targetPos);
-        backLeft.setTargetPosition(backLeft.getCurrentPosition() + targetPos);
-        backRight.setTargetPosition(backRight.getCurrentPosition() + targetPos);
+        rotator.setTargetPosition(rotator.getCurrentPosition() + targetPos);
 
         setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
 
-        while (frontLeft.isBusy() || frontRight.isBusy() || backLeft.isBusy() || backLeft.isBusy()){
+        while (rotator.isBusy()){
             positionReport();
         }
         stop();
@@ -99,16 +77,13 @@ public class EddyAutoBlue {
 
     public void strafeByInch (double inches, double power){
         final int targetPos = (int)(inches * TICKS_PER_INCH);
-        frontLeft.setTargetPosition(frontLeft.getCurrentPosition() - targetPos);
-        frontRight.setTargetPosition(frontRight.getCurrentPosition() + targetPos);
-        backLeft.setTargetPosition(backLeft.getCurrentPosition() + targetPos);
-        backRight.setTargetPosition(backRight.getCurrentPosition() - targetPos);
+        rotator.setTargetPosition(rotator.getCurrentPosition() - targetPos);
 
-        setDrivePowers(power, power, power, power);
+        setDrivePowers(power);
 
         setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
 
-        while (frontLeft.isBusy() || frontRight.isBusy() || backLeft.isBusy() || backLeft.isBusy()){
+        while (rotator.isBusy()){
             positionReport();
         }
         stop();
@@ -116,18 +91,12 @@ public class EddyAutoBlue {
     }
 
     public void positionReport() {
-        telemetry.addData("frontleft ", "%d %d", frontLeft.getTargetPosition(), frontLeft.getCurrentPosition());
-        telemetry.addData("frontright ", "%d %d", frontRight.getTargetPosition(), frontRight.getCurrentPosition());
-        telemetry.addData("backleft ", "%d %d", backLeft.getTargetPosition(), backLeft.getCurrentPosition());
-        telemetry.addData("backright ", "%d %d", backRight.getTargetPosition(), backRight.getCurrentPosition());
+        telemetry.addData("rotator", "%d %d", rotator.getTargetPosition(), rotator.getCurrentPosition());
         telemetry.update();
     }
 
     public void setMode(DcMotorEx.RunMode runMode) {
-        frontLeft.setMode(runMode);
-        frontRight.setMode(runMode);
-        backLeft.setMode(runMode);
-        backRight.setMode(runMode);
+        rotator.setMode(runMode);
     }
 
     public void sleep(int second) {
